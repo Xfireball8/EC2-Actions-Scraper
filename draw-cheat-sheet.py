d8513d6b73fb9714dd1ba413f7d8900fae58d0b2
faisalsalhi <faisal.salhi@etu.univ-smb.fr>
2021-05-26 01:43:33 +0200

    Scrap Amazon EC2 Action Resource Condition web page

--- draw-cheat-sheet.py
+++ draw-cheat-sheet.py
@@ -1,1 +1,6 @@
-print("Hello World !")
+import urllib2
+from bs4 import BeautifulSoup
+
+ec2ResourceActionsConditionPage = urllib2.urlopen('https://docs.aws.amazon.com/service-authorization/latest/reference/list_amazonec2.html#amazonec2-resources-for-iam-policies')
+
+print(ec2ResourceActionsConditionPage)
