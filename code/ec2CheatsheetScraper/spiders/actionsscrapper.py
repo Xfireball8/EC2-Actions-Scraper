8d637e7655598cab00bddab068578350ecd77986
faisalsalhi <faisal.salhi@etu.univ-smb.fr>
2021-05-26 16:47:26 +0200

    Generate Ec2Actions Class and Individuals

--- code/ec2CheatsheetScraper/spiders/actionsscrapper.py
+++ code/ec2CheatsheetScraper/spiders/actionsscrapper.py
@@ -4,15 +4,30 @@ class ActionsScrapper(scrapy.Spider):
     name = 'actionsscraper'
     start_urls = ['https://docs.aws.amazon.com/service-authorization/latest/reference/list_amazonec2.html#amazonec2-resources-for-iam-policies']
     
-    def parse(self, response):
-        # Build Resources Array (Individuals from EC2-Resources class in our ontology)
+    def parse(self, response): 
+        actions = list()
         resources = list()
+        conditions = list()
+        
+        # Build Conditions Array (Indiviudals from Ec2Conditions class in our ontology)
+        for table_row in response.xpath('//table[@id="w87aab5b9d378c15b7"]/tr/td/a[@href]/text()'):
+            conditions.append(table_row.extract().strip())
+
+        # Build Resources Array (Individuals from Ec2Resources class in our ontology)
         for table_row in response.xpath('//table[@id="w87aab5b9d378c13b5"]/tr/td/a[@href]/text()'):
             resources.append(table_row.extract().strip())
         
+        # Build Actions Array (Individuals from Ec2Actions class in our ontology)
+        for table_row in response.xpath('//table[@id="w87aab5b9d378c11b9"]/tr/td/a[@href]/text()'):
+            actions.append(table_row.extract().strip())
+
         # Special attention is given to the nomenclatura behind the dict 
         # resulting from the operation as it is taken as is by the creator
         # script to create Ontology Classes.
         #
         # We want to get ServiceAbbreviation-(Resources|Actions|Conditions)
-        yield { 'EC2-Resources' : resources }
+        # as class name
+        #
+        # Objects are put in an array because json_load can't have multiple object
+        # in top level
+        yield { 'res' : [{'Ec2Actions' : actions}, {'Ec2Resources' : resources}]} #, 'Ec2Conditions' : conditions }
--- code/ontology_creator/creator.py
+++ code/ontology_creator/creator.py
@@ -1,13 +1,14 @@
 import json # Interpreting results from Scrapy
 import os # Check if file exists
+import sys # breakpoints
+
 
 # Create a simple OWL class declaration 
 # Input : rdfId - Name of the class
 # Output : String - Declaration 
 def owlClassTag(rdfId):
     openTag = "<owl:Class rdf:ID=\"" + rdfId + "\" />" + '\n' 
-    closeTag = "</owl:Class>" + '\n'
-    return openTag + closeTag
+    return openTag
 
 # Get a dictionnary from scrapped resources
 # Input : 
@@ -18,8 +19,28 @@ def getResources():
     except OSError:
         print("You need to generate resources with the scrapy spider first !")
         sys.exit()
-    resourcesDictionnary = json.load(resourcesFile)[0] # json.load put my dict in an array
+    # ...[0] <- json.load returns an array with your json accesible in the first row
+    # ...[0]['res'] <- There can be only one top level object in a json read by json.load
+    #                   so 'res' is where there is our array of dict (Actions/Resources/Conditions)
+    #....[0]['res'][1] <- index 1 correspond to our Resource Dict
+    resourcesDictionnary = json.load(resourcesFile)[0]['res'][1] # json.load put my dict in an array
     return resourcesDictionnary
+
+# Get a dictionnary from scrapped actions
+# Input : 
+# Output : actionsDictionnary - Python Dict
+def getActions():
+    try:
+        resourcesFile = open("res/resources.json")
+    except OSError:
+        print("You need to generate resources with the scrapy spider first !")
+        sys.exit()
+    # ...[0] <- json.load returns an array with your json accesible in the first row
+    # ...[0]['res'] <- There can be only one top level object in a json read by json.load
+    #                   so 'res' is where there is our array of dict (Actions/Resources/Conditions)
+    #....[0]['res'][0] <- index 0 correspond to our Action Dict
+    actionsDictionnary = json.load(resourcesFile)[0]['res'][0]
+    return actionsDictionnary
 
 # Open a file to create our ontology, if file
 # is already present, we override it.
@@ -43,19 +64,41 @@ def defineClasses(ontologyFile, resourcesDictionnary):
     for owlClassId in resourcesDictionnary.keys():
         ontologyFile.write(owlClassTag(owlClassId))
 
-# We get the key of our dictionnary that correspond to
-# the OWL Class of our ontology and we define them in
-# our ontology file.
-# Input : ontologyFile - File object
-# Ouput : 
-def createClasses(ontologyFile):
+# Create a simple OWL class declaration 
+# Input : rdfId - Name of the class
+# Output : String - Declaration 
+def owlIndividualTag(owlClassName, rdfId):
+    openTag = "<" + owlClassName  +" rdf:ID=\"" + rdfId + "\" />" + '\n' 
+    return openTag
+
+
+# We create individuals of our ontology
+# Input : ontologyFile - File handle
+#         resourcesDictionnary - Python Dict
+# Output : 
+def defineIndividuals(ontologyFile, resourcesDictionnary):
+    for owlClassId in resourcesDictionnary.keys():
+        individuals = resourcesDictionnary[owlClassId]
+        for owlIndividualId in individuals:
+            ontologyFile.write(owlIndividualTag(owlClassId, owlIndividualId))
+
+# We get the data we just scrapped in a Dict form
+# and we call the functions to build our ontology
+# accordingly to these data
+# Input : ontologyFile - File Handle
+# Output :
+def defineOntology(ontologyFile):
     resourcesDictionnary = getResources()
+    actionsDictionnary = getActions()
     defineClasses(ontologyFile, resourcesDictionnary)
+    defineClasses(ontologyFile, actionsDictionnary)
+    defineIndividuals(ontologyFile, resourcesDictionnary)
+    defineIndividuals(ontologyFile, actionsDictionnary)
 
 # Main function call
 def createOntology():
     ontologyFile = createOntologyFile()
-    createClasses(ontologyFile)
+    defineOntology(ontologyFile)
     ontologyFile.close()
     print("Ontology generation has ended !")
 
