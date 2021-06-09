82b4f4a9c9870c2bfc667f9d4998fd8ef204261c
faisalsalhi <faisal.salhi@etu.univ-smb.fr>
2021-06-09 23:51:47 +0200

    Removed old folder

--- code/ec2CheatsheetScraper/settings.py
+++
@@ -1,8 +0,0 @@
-BOT_NAME = "ec2CheatsheetScraper"
-
-SPIDER_MODULES = ['ec2CheatsheetScraper.spiders']
-NEWSPIDER_MODULE = 'ec2CheatsheetScraper.spiders'
-
-USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0.3 Safari/605.1.15'
-
-ROBOTSTXT_OBEY = True
